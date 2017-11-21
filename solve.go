e8f7d7937050e1d6c69440350a85d2b5224aba5d
SergeyNikiforov <SergeyNikiforov761@gmail.com>
2017-11-21 23:16:45 +0300

    Add files via upload

---
+++ solve.go
@@ -0,0 +1,35 @@
+package main 
+import "fmt"
+import "unicode"
+func RemoveEven(a []int)[]int {
+    s:=make([]int,0,1000)
+    for _,v := range a {
+        if(v%2==1){
+            s = append(s,v)
+        }
+    }
+    return s
+}
+func PowerGenerator(b int) (func() int){
+    res := 1
+    return func() (ret int){
+        ret = res*b
+        res = ret
+        return
+    }
+
+}
+func DifferentWordsCount(c string) int {
+    str := ``
+    x := make(map[string]bool)
+    for i := 0; i < len(c); i++ {
+        for ;(i < len(c) && unicode.IsLetter(rune(c[i]))) ; i++{
+            str = str + string(unicode.ToLower(rune(c[i])))
+        }
+        if(len(str) > 0){
+            x[str] = true;
+        }
+        str = ``
+    }
+    return len(x)
+}
